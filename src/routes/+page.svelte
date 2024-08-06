1edafc409d83f0befa4256e1932c1d532405dd9a
MinNaingKoKo <minnaingkoko2001@gmail.com>
2024-08-06 10:58:16 +0630

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,256 @@
+<script>
+	import logo from '$lib/images/logo.svg';
+	import illustration from '$lib/images/illustration-mockups.svg';
+</script>
+
+<div class="container">
+	<img class="logo" src={logo} alt="" />
+	<div class="row">
+		<img class="left" src={illustration} alt="" />
+		<div class="right">
+			<h1 class="heading">Build The Community Your Fans Will Love</h1>
+			<p class="text">
+				Huddle re-imagines the way we build communities. You have a voice, but so does your
+				audience. Create connections with your users as you engage in genuine discussion.
+			</p>
+			<div class="btn">Register</div>
+		</div>
+	</div>
+	<div class="footer">
+		<div class="link">
+			<svg
+				class="svg-icon"
+				fill="white"
+				height="800px"
+				width="800px"
+				version="1.1"
+				id="Layer_1"
+				xmlns="http://www.w3.org/2000/svg"
+				xmlns:xlink="http://www.w3.org/1999/xlink"
+				viewBox="0 0 310 310"
+				xml:space="preserve"
+			>
+				<g id="XMLID_834_">
+					<path
+						id="XMLID_835_"
+						d="M81.703,165.106h33.981V305c0,2.762,2.238,5,5,5h57.616c2.762,0,5-2.238,5-5V165.765h39.064
+		c2.54,0,4.677-1.906,4.967-4.429l5.933-51.502c0.163-1.417-0.286-2.836-1.234-3.899c-0.949-1.064-2.307-1.673-3.732-1.673h-44.996
+		V71.978c0-9.732,5.24-14.667,15.576-14.667c1.473,0,29.42,0,29.42,0c2.762,0,5-2.239,5-5V5.037c0-2.762-2.238-5-5-5h-40.545
+		C187.467,0.023,186.832,0,185.896,0c-7.035,0-31.488,1.381-50.804,19.151c-21.402,19.692-18.427,43.27-17.716,47.358v37.752H81.703
+		c-2.762,0-5,2.238-5,5v50.844C76.703,162.867,78.941,165.106,81.703,165.106z"
+					/>
+				</g>
+			</svg>
+		</div>
+		<div class="link">
+			<svg
+				class="svg-icon"
+				fill="white"
+				height="800px"
+				width="800px"
+				version="1.1"
+				id="Layer_1"
+				xmlns="http://www.w3.org/2000/svg"
+				xmlns:xlink="http://www.w3.org/1999/xlink"
+				viewBox="0 0 310 310"
+				xml:space="preserve"
+			>
+				<g id="XMLID_826_">
+					<path
+						id="XMLID_827_"
+						d="M302.973,57.388c-4.87,2.16-9.877,3.983-14.993,5.463c6.057-6.85,10.675-14.91,13.494-23.73
+		c0.632-1.977-0.023-4.141-1.648-5.434c-1.623-1.294-3.878-1.449-5.665-0.39c-10.865,6.444-22.587,11.075-34.878,13.783
+		c-12.381-12.098-29.197-18.983-46.581-18.983c-36.695,0-66.549,29.853-66.549,66.547c0,2.89,0.183,5.764,0.545,8.598
+		C101.163,99.244,58.83,76.863,29.76,41.204c-1.036-1.271-2.632-1.956-4.266-1.825c-1.635,0.128-3.104,1.05-3.93,2.467
+		c-5.896,10.117-9.013,21.688-9.013,33.461c0,16.035,5.725,31.249,15.838,43.137c-3.075-1.065-6.059-2.396-8.907-3.977
+		c-1.529-0.851-3.395-0.838-4.914,0.033c-1.52,0.871-2.473,2.473-2.513,4.224c-0.007,0.295-0.007,0.59-0.007,0.889
+		c0,23.935,12.882,45.484,32.577,57.229c-1.692-0.169-3.383-0.414-5.063-0.735c-1.732-0.331-3.513,0.276-4.681,1.597
+		c-1.17,1.32-1.557,3.16-1.018,4.84c7.29,22.76,26.059,39.501,48.749,44.605c-18.819,11.787-40.34,17.961-62.932,17.961
+		c-4.714,0-9.455-0.277-14.095-0.826c-2.305-0.274-4.509,1.087-5.294,3.279c-0.785,2.193,0.047,4.638,2.008,5.895
+		c29.023,18.609,62.582,28.445,97.047,28.445c67.754,0,110.139-31.95,133.764-58.753c29.46-33.421,46.356-77.658,46.356-121.367
+		c0-1.826-0.028-3.67-0.084-5.508c11.623-8.757,21.63-19.355,29.773-31.536c1.237-1.85,1.103-4.295-0.33-5.998
+		C307.394,57.037,305.009,56.486,302.973,57.388z"
+					/>
+				</g>
+			</svg>
+		</div>
+		<div class="link">
+			<svg
+				class="svg-icon"
+				width="800px"
+				height="800px"
+				viewBox="0 0 24 24"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+					d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"
+					fill="white"
+				/>
+				<path
+					d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z"
+					fill="white"
+				/>
+				<path
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+					d="M1.65396 4.27606C1 5.55953 1 7.23969 1 10.6V13.4C1 16.7603 1 18.4405 1.65396 19.7239C2.2292 20.8529 3.14708 21.7708 4.27606 22.346C5.55953 23 7.23969 23 10.6 23H13.4C16.7603 23 18.4405 23 19.7239 22.346C20.8529 21.7708 21.7708 20.8529 22.346 19.7239C23 18.4405 23 16.7603 23 13.4V10.6C23 7.23969 23 5.55953 22.346 4.27606C21.7708 3.14708 20.8529 2.2292 19.7239 1.65396C18.4405 1 16.7603 1 13.4 1H10.6C7.23969 1 5.55953 1 4.27606 1.65396C3.14708 2.2292 2.2292 3.14708 1.65396 4.27606ZM13.4 3H10.6C8.88684 3 7.72225 3.00156 6.82208 3.0751C5.94524 3.14674 5.49684 3.27659 5.18404 3.43597C4.43139 3.81947 3.81947 4.43139 3.43597 5.18404C3.27659 5.49684 3.14674 5.94524 3.0751 6.82208C3.00156 7.72225 3 8.88684 3 10.6V13.4C3 15.1132 3.00156 16.2777 3.0751 17.1779C3.14674 18.0548 3.27659 18.5032 3.43597 18.816C3.81947 19.5686 4.43139 20.1805 5.18404 20.564C5.49684 20.7234 5.94524 20.8533 6.82208 20.9249C7.72225 20.9984 8.88684 21 10.6 21H13.4C15.1132 21 16.2777 20.9984 17.1779 20.9249C18.0548 20.8533 18.5032 20.7234 18.816 20.564C19.5686 20.1805 20.1805 19.5686 20.564 18.816C20.7234 18.5032 20.8533 18.0548 20.9249 17.1779C20.9984 16.2777 21 15.1132 21 13.4V10.6C21 8.88684 20.9984 7.72225 20.9249 6.82208C20.8533 5.94524 20.7234 5.49684 20.564 5.18404C20.1805 4.43139 19.5686 3.81947 18.816 3.43597C18.5032 3.27659 18.0548 3.14674 17.1779 3.0751C16.2777 3.00156 15.1132 3 13.4 3Z"
+					fill="white"
+				/>
+			</svg>
+		</div>
+	</div>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		height: 100vh;
+		flex-direction: column;
+		position: relative;
+	}
+	.logo {
+		margin-top: 64px;
+		margin-left: 80px;
+		width: 200px;
+		height: 32px;
+		margin-bottom: 104px;
+	}
+	.row {
+		display: flex;
+		flex-direction: row;
+		gap: 59px;
+		align-self: center;
+	}
+	.left {
+		width: 696px;
+		height: 493px;
+		object-fit: cover;
+	}
+	.right {
+		display: flex;
+		flex-direction: column;
+		gap: 24px;
+	}
+	.heading {
+		width: 487px;
+		font-size: 40px;
+		line-height: 60px;
+		font-weight: 600;
+		color: #ffffff;
+	}
+	.text {
+		width: 520px;
+		font-size: 18px;
+		line-height: 27px;
+		color: white;
+	}
+	.btn {
+		width: 200px;
+		height: 56px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border-radius: 28px;
+		background-color: white;
+		color: #684bb1;
+		font-size: 18px;
+		line-height: 27px;
+		cursor: pointer;
+		transition: 0.3s ease-out;
+	}
+	.btn:hover {
+		background-color: #e880e8;
+		color: white;
+	}
+	.footer {
+		display: flex;
+		flex-direction: row;
+		position: absolute;
+		right: 80px;
+		bottom: 40px;
+		gap: 16px;
+	}
+	.svg-icon {
+		width: 18px;
+		height: 18px;
+		transition: 0.3s ease-out;
+	}
+	.link {
+		cursor: pointer;
+		width: 40px;
+		height: 40px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border: 1px solid white;
+		border-radius: 100%;
+		transition: 0.3s ease-out;
+	}
+	.link:hover {
+		border: 1px solid #ff52c1;
+	}
+	.link:hover .svg-icon path {
+		fill: #ff52c1;
+	}
+	@media only screen and (max-width: 375px) {
+		.logo {
+			margin-top: 38.2px;
+			margin-left: 40px;
+			width: 120px;
+			height: 19px;
+			margin-bottom: 69.8px;
+		}
+		.row {
+			flex-direction: column;
+			gap: 60px;
+			align-items: center;
+		}
+		.left {
+			width: 296px;
+			height: 209px;
+		}
+		.right {
+			align-items: center;
+			gap: 0;
+			text-align: center;
+		}
+		.heading {
+			width: 280px;
+			font-size: 24px;
+			line-height: 36px;
+			margin-bottom: 16px;
+		}
+		.text {
+			width: 304px;
+			font-size: 16px;
+			line-height: 24px;
+			margin-bottom: 24px;
+		}
+		.btn {
+			height: 40px;
+			font-size: 12px;
+			line-height: 18px;
+		}
+		.btn:hover {
+			background-color: #e880e8;
+			color: white;
+		}
+		.footer {
+			margin-top: 64px;
+			align-self: center;
+			position: static;
+			gap: 10.49px;
+		}
+		.svg-icon {
+			width: 12px;
+			height: 12px;
+		}
+		.link {
+			width: 28px;
+			height: 28px;
+		}
+	}
+</style>
